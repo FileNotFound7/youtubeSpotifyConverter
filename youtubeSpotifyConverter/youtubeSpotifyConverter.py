80c8154e598bb51f4b8d55da0e3ba267e7640d7a
FileNotFound7 <111416523+FileNotFound7@users.noreply.github.com>
2023-10-12 20:06:48 +1100

    add methods to use for oauth2 for spotify untested

--- youtubeSpotifyConverter/youtubeSpotifyConverter.py
+++ youtubeSpotifyConverter/youtubeSpotifyConverter.py
@@ -1,46 +1,104 @@
 import requests
 from base64 import b64encode
 from urllib.parse import urlparse, parse_qs, urlencode
+from time import time
 
 class youtubeSpotifyConverter:
     # the URLs used to make requests
     SP_URL = "https://api.spotify.com/v1/"
     YT_URL = "https://youtube.googleapis.com/youtube/v3/"
 
-    def __init__(self, YT_API_KEY:str, SP_CLIENT_ID:str, SP_CLIENT_SECRET:str):
+    def __init__(self, YT_API_KEY:str=None, SP_REFRESH_TOKEN:str=None, SP_CLIENT_ID:str=None, SP_CLIENT_SECRET:str=None):
         """
-        Sets the api keys.
+        Sets the api keys. If no arguments are passed, nothing will work.
 
-        :param YT_API_KEY: The youtube API key.
+        :param YT_API_KEY The youtube API key. (optional if you don't want youtube or converter functions)
         :type YT_API_KEY: str
 
-        :param SP_CLIENT_ID: The spotify API client id.
+        :param SP_REFRESH_TOKEN: The spotify API refresh token which belongs to a particular user. (optional if you don't need access to a user account or aren't using spotify functions)
         :type SP_CLIENT_ID: str
 
-        :param SP_CLIENT_SECRET: The spotify API client secret.
+        :param SP_CLIENT_ID: The spotify API client id. (optional if you don't want spotify or converter functions or are using a refresh token)
+        :type SP_CLIENT_ID: str
+
+        :param SP_CLIENT_SECRET: The spotify API client secret. (optional if you don't want spotify or converter functions or are using a refresh token)
         :type SP_CLIENT_SECRET: str
 
         """
         
-        # this will be used always for youtube API requests
-        self.YT_key = YT_API_KEY
+        # this takes a look at all arguments passed and decides what methods are usable from that
+        if YT_API_KEY != None:
+            # this key will be used always for youtube API requests
+            self.YT_key = YT_API_KEY
 
-        # these two will not be used after getting a token from the spotify API
-        self.SP_id = SP_CLIENT_ID
-        self.SP_secret = SP_CLIENT_SECRET
+            self.converter = True
+            self.youtube = True
+        else:
+            self.converter = False
+            self.youtube = False
+        
+        if SP_REFRESH_TOKEN != None:
+            self.SP_REFRESH_TOKEN = SP_REFRESH_TOKEN
 
-        # get an token for the spotify API
-        self.__SP_authorise()
+            self.spotify_mode = 1 # this indicates how spotify is being accessed, whether by user token (1) or using client credentials (2) or with no spotify access (0)
+        elif SP_CLIENT_ID != None and SP_CLIENT_SECRET != None:
+            # these keys are only used to get an access token
+            self.SP_CLIENT_ID = SP_CLIENT_ID
+            self.SP_CLIENT_SECRET = SP_CLIENT_SECRET
 
-    def __SP_authorise(self):
+            self.spotify_mode = 2
+        else:
+            self.spotify_mode = 0
+            self.converter = False
+
+        self.timer = 0
+        self.SP_getToken()
+
+    def SP_getToken(self):
+        if self.spotify_mode != 0:
+            if time - self.timer >= 3600:
+                if self.spotify_mode == 1:
+                    token = self.__SP_refreshToken()
+                elif self.spotify_mode == 2:
+                    token = self.__SP_clientCreds()
+                
+                self.SP_token = token
+                return token
+            else:
+                return self.SP_token
+
+    def __SP_refreshToken(self):
         """
-        gets a token for making requests to the spotify API
+        gets a token for making requests to the spotify API via refresh token
+        """
+    
+        url = "https://accounts.spotify.com/api/token" # token request url
+
+        # converts the client id and secret in base64 that spotify will understand
+        credentials = self.SP_CLIENT_ID + ":" + self.SP_CLIENT_SECRET
+        credentials = credentials.encode("ascii")
+        credentials = b64encode(credentials).decode()
+
+        # the request data
+        headers = {'Authorization': 'Basic ' + str(credentials), "Content-Type": "application/x-www-form-urlencoded"}
+        params = {"grant_type" : "authorization_code", "code" : self.SP_REFRESH_TOKEN, "redirect_uri": self.SP_REDIRECT_URI}
+
+        # send the request
+        response = requests.post(url=url,headers=headers, params=params)
+
+        # raise an exception if the request has a bad status code if not return the token
+        response.raise_for_status()
+        return(response.json()["access_token"])
+
+    def __SP_clientCreds(self):
+        """
+        gets a token for making requests to the spotify API via client credentials.
         """
 
         url = "https://accounts.spotify.com/api/token" # token request url
 
         # converts the client id and secret in base64 that spotify will understand
-        credentials = self.SP_id + ":" + self.SP_secret
+        credentials = self.SP_CLIENT_ID + ":" + self.SP_CLIENT_ID
         credentials = credentials.encode("ascii")
         credentials = b64encode(credentials).decode()
 
@@ -51,17 +109,14 @@ class youtubeSpotifyConverter:
         # send the request
         response = requests.post(url=url,headers=headers, params=params)
         
-        # raise an exception if the request fails or else save the token
-        if response.status_code == 200:
-            self.SP_token = response.json()["access_token"]
-        else:
-            raise Exception(f"Error response after requesting token.\nResponse status code: {response.status_code}\nResponse Content: {response.content}")
+        # raise an exception if the request has a bad status code if not return the token
+        response.raise_for_status()
+        return(response.json()["access_token"])
 
-    def SP_userauth(self, scope:str, redirect_uri:str, state):
+    def SP_userAuth(self, scope:str, redirect_uri:str, state):
         
         url = "https://accounts.spotify.com/authorize?"
 
-        headers = {'Authorization': f'Bearer {self.SP_token}'}
         params = {"client_id": self.SP_id,
                   "response_type": "code",
                   "redirect_uri": redirect_uri,
@@ -69,7 +124,63 @@ class youtubeSpotifyConverter:
                   "scope": scope
                   }
                    
-        print(url + urlencode(params))
+        return(url + urlencode(params))
+
+    def SP_getToken(self, code:str, redirect_uri:str):
+    
+        url = "https://accounts.spotify.com/api/token" # token request url
+
+        # converts the client id and secret in base64 that spotify will understand
+        credentials = self.SP_CLIENT_ID + ":" + self.SP_CLIENT_SECRET
+        credentials = credentials.encode("ascii")
+        credentials = b64encode(credentials).decode()
+
+        # the request data
+        headers = {'Authorization': 'Basic ' + str(credentials), "Content-Type": "application/x-www-form-urlencoded"}
+        params = {"grant_type" : "authorization_code", "code" : self.SP_REFRESH_TOKEN, "redirect_uri": self.SP_REDIRECT_URI}
+
+        # send the request
+        response = requests.post(url=url,headers=headers, params=params)
+        
+        # raise an exception if the request has a bad status code if not return the token
+        response.raise_for_status()
+        return(response.json()["access_token"])
+
+    def SP_createPlaylist(self, name:str, description:str="", public:bool=True):
+        """
+        creates a spotify playlist
+
+        :param token: The token to 
+        :type description: str
+
+        :param name: The name of the new playlist
+        :type name: str
+        
+        :param description: The description of the new playlist
+        :type description: str
+
+        :param public: If the playlist is public or not (defaults to True)
+        :type public: bool
+        """
+        
+        headers = {
+            "Content-Type" : "application/json",
+            "Authorization" : f"Bearer {self.SP_token}"
+        }
+
+        params = {
+            "name" : name,
+            "description" : description,
+            "public" : public,
+        }
+
+        url=f"{self.SP_URL}users/{id}/playlists"
+
+        response = requests.get(url=url, headers=headers, params=params)
+        
+        # raise an exception if the request has a bad status code if not return
+        response.raise_for_status()
+        return(response.json())
 
     def SP_search(self, keyword:str, limit:int=1):
         """
@@ -97,7 +208,9 @@ class youtubeSpotifyConverter:
         }
 
         response = requests.get(url=self.SP_URL+"search/", headers=headers, params=params)
-
+        
+        # raise an exception if the request has a bad status code if not return
+        response.raise_for_status()
         return(response.json())
 
     def SP_get(self, id:str):
@@ -120,37 +233,8 @@ class youtubeSpotifyConverter:
 
         response = requests.get(url=url, headers=headers)
 
-        return(response.json())
-
-    def SP_createPlaylist(self, id:int, name:str, description:str="", public:bool=True):
-        """
-        creates a spotify playlist
-
-        :param name: The name of the new playlist
-        :type name: str
-        
-        :param description: The description of the new playlist
-        :type description: str
-
-        :param public: If the playlist is public or not (defaults to True)
-        :type public: bool
-        """
-        
-        headers = {
-            "Content-Type" : "application/json",
-            "Authorization" : f"Bearer {self.SP_token}"
-        }
-
-        params = {
-            "name" : name,
-            "description" : description,
-            "public" : public,
-        }
-
-        url=f"{self.SP_URL}users/{id}/playlists"
-
-        response = requests.get(url=url, headers=headers, params=params)
-
+        # raise an exception if the request has a bad status code if not return
+        response.raise_for_status()
         return(response.json())
 
     def YT_search(self, keyword: str, limit=1):
@@ -168,6 +252,9 @@ class youtubeSpotifyConverter:
         """
 
         response = requests.get(self.YT_URL+f"search?part=snippet&maxResults={limit}&q={keyword}&key={self.YT_key}")
+
+        # raise an exception if the request has a bad status code if not return
+        response.raise_for_status()
         return(response.json())
 
     def YT_getVideo(self, id:str):
@@ -182,6 +269,9 @@ class youtubeSpotifyConverter:
         """
 
         response = requests.get(self.YT_URL+f"videos?part=snippet%2CcontentDetails%2Cstatistics&id={id}&key={self.YT_key}")
+        
+        # raise an exception if the request has a bad status code if not return
+        response.raise_for_status()
         return response.json()
 
     def YT_getChannel(self, id:str):
@@ -196,6 +286,9 @@ class youtubeSpotifyConverter:
         """
 
         response = requests.get(self.YT_URL+f"channels?part=snippet%2CcontentDetails%2Cstatistics&id={id}&key={self.YT_key}")
+        
+        # raise an exception if the request has a bad status code if not return
+        response.raise_for_status()
         return response.json()
 
     def YT_intoMusic(self, URL:str):
